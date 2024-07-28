ead0f9d5bf64c2df8953131befc56f2f19263fce
Azim Ahmed Warsi <azimahmedwarsi3115@gmail.com>
2024-07-28 23:38:11 +0530

    FIX1

--- src/components/homepage/Contact.jsx
+++ src/components/homepage/Contact.jsx
@@ -74,7 +74,8 @@ export default function Contact() {
             Have an awesome idea? Let&apos;s bring it to life.
           </h3>
           <p ref={body} className="mt-4 max-w-md 2xl:max-w-2xl text-body-2 2xl:text-4xl text-accent-100 translate-y-10 opacity-0">
-            I am currently not available for freelance work. I am accepting new projects starting from February 2022.
+            I am currently available for freelance work. 
+            {/* I am accepting new projects starting from February 2022. */}
           </p>
           <form
             name="contact"
